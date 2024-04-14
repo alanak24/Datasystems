57e439d4aba64106d347730c6ac6f70e2a889102
jkimumali <jaemiely.k.umali@student.uts.edu.au>
2024-04-14 21:25:09 +1000

    environment set up and initial instructions

---
+++ main.py
@@ -0,0 +1,11 @@
+import os, uuid
+from dotenv import load_dotenv
+from master.data_setup import *
+from master.dim_setup import *
+
+def main():
+    while True:
+        print("main")
+
+if __name__ == '__main__':
+    main()
